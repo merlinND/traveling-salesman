98b5ae3e1cb5f12acd9bcd39fc2b2fdc999098bd
Robin Ricard <ricard.robin@gmail.com>
2014-03-31 20:37:21 +0200

    Not finished yet, but getting better !

--- src/opt_locale.cpp
+++ src/opt_locale.cpp
@@ -34,19 +34,12 @@ t_cycle two_opt(int indexVilleOpt, int nbVilles, double ** distances, t_cycle cy
     }
   }
   t_cycle cycle_local = pvc_exact_branch_and_bound(taille_locale, distances_locales);
-  for(int i = 0; i < taille_locale; i ++)
-  {
-    for(int j = 0; j < taille_locale; i ++) {
 
-    }
+  for(int i = 0; i < taille_locale; i ++) {
+    cycle.c[debut + i] = cycle.c[debut + cycle_local.c[i]];
     free(distances_locales[i]);
   }
   free(distances_locales);
-
-  for(int i = 0; i < taille_locale; i ++) {
-    cycle.c[debut + i] = cycle_local.c[i];
-  }
-
   return cycle;
 }
 
